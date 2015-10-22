8298a69c9096f38a438bb523c0534b7178f9f24e
Kedreals <ebert.mueden1@freenet.de>
2015-10-22 14:38:05 +0200

    Vorbereitung von dem Life Coding der dritten Veranstaltung neues project einbinden von SFML und schon mal ein Fenster erzeugt, der Rest dann Life

---
+++ 3. Vorlesung 03.11.14/Intro2D-03-Beispiel-Player-Enemy/Intro2D-03-Beispiel-Player-Enemy/Program.cs
@@ -0,0 +1,46 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro2D_03_Beispiel_Player_Enemy
+{
+    //ToDo: write classes for Player and Enemy
+
+    class Program
+    {
+        //ToDo: Declare Player and two Enemies
+
+        static void Main(string[] args)
+        {
+            RenderWindow win = new RenderWindow(new VideoMode(800, 600), "Intro2D-03-Beispiel-Player-Enemy");
+            win.Closed += (sender, e) => { ((RenderWindow)sender).Close(); };
+
+            //ToDo: Initialize Player and Enemies
+
+            while (win.IsOpen())
+            {
+                Update();
+                Draw(win);
+                win.DispatchEvents();
+            } 
+        }
+
+        static void Draw(RenderWindow window)
+        {
+            window.Clear(new Color(50, 120, 190));
+
+            //ToDo: Draw Player and Enemies
+
+            window.Display();
+        }
+
+        static void Update()
+        {
+            //ToDo: Update Player and Enemies
+        }
+    }
+}
