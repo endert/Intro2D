a453c005b2255aa48227d423847f86edd5486465
Kedreals <ebert.mueden1@freenet.de>
2015-11-03 12:36:28 +0100

    Commented Code

---
+++ 3. Vorlesung 28.10.15/Intro2D-03-Beispiel-Player-Enemy/Intro2D-03-Beispiel-Player-Enemy/Enemy.cs
@@ -0,0 +1,61 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro2D_03_Beispiel_Player_Enemy
+{
+    /// <summary>
+    /// the enemy
+    /// </summary>
+    class Enemy
+    {
+        //attributes
+        Texture textur;
+        Sprite sprite;
+        
+        /// <summary>
+        /// initializes a new Enemy
+        /// </summary>
+        /// <param name="direction">the direction to the texture as a string</param>
+        /// <param name="sPosition">start position</param>
+        public Enemy(string direction, Vector2f sPosition)
+        {
+            textur = new Texture(direction);
+            sprite = new Sprite(textur);
+
+            sprite.Position = sPosition;
+        } 
+
+        /// <summary>
+        /// moves in the given direction
+        /// </summary>
+        /// <param name="playerPos"></param>
+        public void Move(Vector2f playerPos)
+        {
+            //evaluate the direction from this position to the given one. Math \(^^)/
+            Vector2f direction = playerPos - sprite.Position;
+
+            //evaluate the length of the direction vector. Math \(^^)/
+            float length = (float)Math.Sqrt(direction.X * direction.X + direction.Y * direction.Y);
+
+            //norming the direction vector, so it have the length of 1. Math \(^^)/
+            direction = direction / length;
+
+            //adding a percentage of the direction to the position. guess what comes now^^ Math \(^^)/
+            sprite.Position += direction*0.01f;
+        }
+
+        /// <summary>
+        /// draws the sprite in the given window
+        /// </summary>
+        /// <param name="win"></param>
+        public void Draw(RenderWindow win)
+        {
+            win.Draw(sprite);
+        }
+    }
+}
--- 3. Vorlesung 28.10.15/Intro2D-03-Beispiel-Player-Enemy/Intro2D-03-Beispiel-Player-Enemy/Program.cs
+++ 3. Vorlesung 28.10.15/Intro2D-03-Beispiel-Player-Enemy/Intro2D-03-Beispiel-Player-Enemy/Program.cs
@@ -1,4 +1,6 @@
-﻿using SFML.Graphics;
+﻿//wenn ihr Probleme mit englischen Kommentaren habt, dann beschwert euch in der nächsten Vorlesung, bei eurem Englisch Lehrer.
+
+using SFML.Graphics;
 using SFML.Window;
 using System;
 using System.Collections.Generic;
@@ -8,18 +10,22 @@ using System.Threading.Tasks;
 
 namespace Intro2D_03_Beispiel_Player_Enemy
 {
-    //ToDo: write classes for Player and Enemy
-
     class Program
     {
-        //ToDo: Declare Player and two Enemies
+        //declare Players and Enemies
+        static Player player;
+        static Enemy enemy1;
+        static Enemy enemy2;
 
         static void Main(string[] args)
         {
             RenderWindow win = new RenderWindow(new VideoMode(800, 600), "Intro2D-03-Beispiel-Player-Enemy");
             win.Closed += (sender, e) => { ((RenderWindow)sender).Close(); };
 
-            //ToDo: Initialize Player and Enemies
+            //initialize Player and Enemies, by calling the constructors
+            player = new Player();
+            enemy1 = new Enemy("Pictures/EnemyGreen.png", new Vector2f(900,100));
+            enemy2 = new Enemy("Pictures/EnemyRed.png", new Vector2f(100,600) );
 
             while (win.IsOpen())
             {
@@ -29,18 +35,30 @@ namespace Intro2D_03_Beispiel_Player_Enemy
             } 
         }
 
+        /// <summary>
+        /// clear the window
+        /// <para>draws all gameobjects in the window</para>
+        /// <para>displays all drawn objects</para>
+        /// </summary>
         static void Draw(RenderWindow window)
         {
             window.Clear(new Color(50, 120, 190));
 
-            //ToDo: Draw Player and Enemies
+            player.Draw(window);
+            enemy1.Draw(window);
+            enemy2.Draw(window);
 
             window.Display();
         }
 
+        /// <summary>
+        /// updates all Gameobjects
+        /// </summary>
         static void Update()
         {
-            //ToDo: Update Player and Enemies
+            player.Move();
+            enemy1.Move(player.GetPosition());
+            enemy2.Move(player.GetPosition());
         }
     }
 }
