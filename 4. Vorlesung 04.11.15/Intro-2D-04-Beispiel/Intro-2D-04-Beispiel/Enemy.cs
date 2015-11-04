bbddfc1aaf95138b2c2188ba013ee7dff06c9fb2
Kedreals <ebert.mueden1@freenet.de>
2015-11-04 12:23:58 +0100

    Live Coding ist nun vorbereitet

---
+++ 4. Vorlesung 04.11.15/Intro-2D-04-Beispiel/Intro-2D-04-Beispiel/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro_2D_04_Beispiel
+{
+    /// <summary>
+    /// the enemy
+    /// </summary>
+    class Enemy : GameObject
+    {
+        /// <summary>
+        /// initializes a new Enemy
+        /// </summary>
+        /// <param name="direction">the direction to the texture as a string</param>
+        /// <param name="sPosition">start position</param>
+        public Enemy(string direction, Vector2f sPosition)
+        {
+            textur = new Texture(direction);
+            sprite = new Sprite(textur);
+            MovementSpeed = 0.1f;
+
+            sprite.Position = sPosition;
+        }
+
+        /// <summary>
+        /// calls the move Methode
+        /// </summary>
+        public override void Update()
+        {
+            MovingDirection = Program.Player.Position - sprite.Position;
+            Move();
+        }
+    }
+}
---
+++ 4. Vorlesung 04.11.15/Intro-2D-04-Beispiel/Intro-2D-04-Beispiel/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Intro_2D_04_Beispiel
+{
+    class Map
+    {
+       
+    }
+}
