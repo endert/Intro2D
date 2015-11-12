e8051c3471558ea049485d30f504638a1e3f322e
Kedreals <ebert.mueden1@freenet.de>
2015-11-12 14:14:37 +0100

    Live Coding commentiert

--- 5. Vorlesung 11.11.15/Intro-2D-05-Beispiel/Intro-2D-05-Beispiel/Enemy.cs
+++ 5. Vorlesung 11.11.15/Intro-2D-05-Beispiel/Intro-2D-05-Beispiel/Enemy.cs
@@ -13,6 +13,7 @@ namespace Intro_2D_05_Beispiel
     /// </summary>
     class Enemy : GameObject
     {
+        //needed for the animation
         Texture textur2;
 
         /// <summary>
@@ -29,9 +30,28 @@ namespace Intro_2D_05_Beispiel
             sprite.Position = sPosition;
         }
 
+        /// <summary>
+        /// overloaded constructor
+        /// <para>programm calls the best fitting one</para>
+        /// </summary>
+        /// <param name="direction"></param>
+        /// <param name="sPos"></param>
+        /// <param name="direction2"></param>
         public Enemy(string direction, Vector2f sPos, string direction2) : this(direction, sPos)
         {
             textur2 = new Texture(direction2);
+        }
+
+        /// <summary>
+        /// animates the enemy by switching the texture of the sprite
+        /// </summary>
+        /// <param name="gTime"></param>
+        protected void Animate(GameTime gTime)
+        {
+            if (gTime.Total.Milliseconds % 1000 < 500)
+                sprite.Texture = textur;
+            else
+                sprite.Texture = textur2;
         }
 
         /// <summary>
@@ -42,7 +62,8 @@ namespace Intro_2D_05_Beispiel
             movementSpeed = baseMovementSpeed * gTime.Ellapsed.Milliseconds;
             MovingDirection = Program.Player.Position - sprite.Position;
             Move();
-            //Animate(gTime);
+            if (isMoving)
+                Animate(gTime);
         }
     }
 }
--- 5. Vorlesung 11.11.15/Intro-2D-05-Beispiel/Intro-2D-05-Beispiel/GameTime.cs
+++ 5. Vorlesung 11.11.15/Intro-2D-05-Beispiel/Intro-2D-05-Beispiel/GameTime.cs
@@ -7,17 +7,27 @@ using System.Threading.Tasks;
 
 namespace Intro_2D_05_Beispiel
 {
+    /// <summary>
+    /// class that contains infos about the time
+    /// </summary>
     class GameTime
     {
+        //the watch for measuring the time
         Stopwatch watch;
-        public TimeSpan Ellapsed { get; private set; }
+        /// <summary>
+        /// System.TimeSpan that represents the total Time that is passed since initializing this instanz
+        /// </summary>
         public TimeSpan Total { get; private set; }
+        /// <summary>
+        /// System.TimeSpan that represents the time, that is needed to pass throught the gameloop once
+        /// </summary>
+        public TimeSpan Ellapsed { get; private set; }
 
         public GameTime()
         {
             watch = new Stopwatch();
-            Ellapsed = new TimeSpan();
             Total = new TimeSpan();
+            Ellapsed = new TimeSpan();
 
             watch.Start();
         }
