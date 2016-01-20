023b905d1ec3264a2e68d29bd4dd7cfb2b9f1b35
Kedreals <ebert.mueden1@freenet.de>
2016-01-20 17:08:20 +0100

    Stuff

---
+++ 12. Vorlesung 20.01.16/Intro2D-12-Beispiel/Intro2D-12-Beispiel/Game.cs
@@ -0,0 +1,88 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro2D_12_Beispiel
+{
+    class Game
+    {
+        RenderWindow win;
+        string Path = "Saves/PlayerData.end";
+        Player p;
+
+        public Game()
+        {
+            win = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Load/ Save");
+            win.Closed += (sender, e) => { ((RenderWindow)sender).Close(); };
+
+            Load();
+        }
+
+        void Load()
+        {
+            if (!Directory.Exists("Saves"))
+                p = new Player();
+            else if (!File.Exists(Path))
+                p = new Player();
+            else
+            {
+                using(StreamReader reader = new StreamReader(Path))
+                {
+                    float hp = Convert.ToSingle(reader.ReadLine());
+                    float x = Convert.ToSingle(reader.ReadLine());
+                    float y = Convert.ToSingle(reader.ReadLine());
+
+                    Vector2f pos = new Vector2f(x, y);
+
+                    p = new Player(hp, pos);
+                }
+            }
+        }
+
+        void Save()
+        {
+            if (!Directory.Exists("Saves"))
+                Directory.CreateDirectory("Saves");
+
+            using (StreamWriter writer = new StreamWriter(Path))
+            {
+                writer.WriteLine(p.Hp);
+                writer.WriteLine(p.Position.X);
+                writer.WriteLine(p.Position.Y);
+            }
+        }
+
+        public void Run()
+        {
+            while (win.IsOpen())
+            {
+                Update();
+                Draw();
+                
+                win.DispatchEvents();
+            }
+        }
+
+        void Update()
+        {
+            p.Update();
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+            {
+                Save();
+                win.Close();
+            }
+        }
+
+        void Draw()
+        {
+            win.Clear();
+            p.Draw(win);
+            win.Display();
+        }
+    }
+}
---
+++ 12. Vorlesung 20.01.16/Intro2D-12-Beispiel/Intro2D-12-Beispiel/Player.cs
@@ -0,0 +1,53 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro2D_12_Beispiel
+{
+    class Player
+    {
+        RectangleShape s;
+        public float Hp { get; private set; }
+        float MaxHp = 200;
+        public Vector2f Position { get { return s.Position; } set { s.Position = value; } }
+
+        public Player(float hp = 200)
+        {
+            s = new RectangleShape(new Vector2f(100, 200));
+            Hp = hp;
+        }
+
+        public Player(float hp, Vector2f pos) : this(hp)
+        {
+            s.Position = pos;
+        }
+
+        public void Draw(RenderWindow win)
+        {
+            win.Draw(s);
+        }
+
+        public void Update()
+        {
+            s.Scale = new Vector2f(Hp / MaxHp, Hp / MaxHp);
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+                s.Position += new Vector2f(0.1f, 0);
+            if (Keyboard.IsKeyPressed(Keyboard.Key.I))
+                s.Position += new Vector2f(0, 0.1f);
+            if (Keyboard.IsKeyPressed(Keyboard.Key.U))
+                s.Position += new Vector2f(-0.1f, 0);
+            if (Keyboard.IsKeyPressed(Keyboard.Key.V))
+                s.Position += new Vector2f(0, -0.1f);
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.G))
+                Hp += 0.1f;
+            if (Keyboard.IsKeyPressed(Keyboard.Key.R))
+                Hp -= 0.1f;
+        }
+    }
+}
